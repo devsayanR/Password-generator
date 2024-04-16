4eadafded2189754323efa736768d53f159dca45
devSayanR <sayanrakshit.prof@gmail.com>
2024-04-16 19:25:30 +0530

    UI changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState,useEffect } from 'react'
+import { useCallback, useRef, useState, useEffect } from 'react'
 
 function App() {
   const [password, setPassword] = useState("")
@@ -28,7 +28,7 @@ function App() {
     document.getElementById('copy-btn').innerText = 'Copied'
     setTimeout(() => {
       document.getElementById('copy-btn').innerText = 'Copy'
-    },1000)
+    }, 1000)
   }, [password])
 
   useEffect(() => {
@@ -38,26 +38,33 @@ function App() {
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
-        <h1 className='text-white text-center pb-4'>Password Generator</h1>
-        <div className='flex justify-center shadow rounded-lg overflow-hidden mb-4'>
-          <input type="text"
-            value={password}
-            className='outline-none w-full py-1 px-3'
-            placeholder='Password'
-            readOnly
-          />
-          <button id='copy-btn'  className='bg-[#1D4ED8] text-white px-2' onClick={() => copyPasswordToClipboard()}>Copy</button>
-        </div>
-        <div className='flex items-center'>
-          <input type="range" min={6} max={100} value={length} className='cursor-pointer outline-none border-none' onChange={(e) => setLength(e.target.value)} ref={passwordRef} />
-          <label className='ml-1 cursor-pointer'>Length: {length}</label>
+      <div className='h-screen flex items-center justify-center'>
+        <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-[#02131A] text-orange-500'>
+          <h1 className='text-white text-center pb-4'>Password Generator</h1>
+          <div className='flex justify-center shadow rounded-lg overflow-hidden mb-4'>
+            <input type="text"
+              value={password}
+              className='outline-none w-full py-1 px-3'
+              placeholder='Password'
+              readOnly
+            />
+          </div>
+          <div className='flex item-center justify-between '>
+            <input type="range" min={6} max={100} value={length} className='cursor-pointer outline-none border-none w-[70%]' onChange={(e) => setLength(e.target.value)} ref={passwordRef} />
+            <label className='ml-1 cursor-pointer'>Length: {length}</label>
+          </div>
 
-          <input type="checkbox" id='numbers' defaultChecked={numberAllowed} onChange={() => setNumberAllowed((prev) => !prev)} className='ml-3 cursor-pointer' />
-          <label htmlFor='numbers' className='ml-1  cursor-pointer'>Numbers</label>
-
-          <input type="checkbox" id='chars' defaultChecked={charactersAllowed} onChange={() => setCharactersAllowed((prev) => !prev)} className='ml-3 cursor-pointer' />
-          <label htmlFor='chars' className='ml-1 cursor-pointer'>Characters</label>
+          <div className='pt-4'>
+            <div className='mb-3'>
+              <input type="checkbox" id='numbers' defaultChecked={numberAllowed} onChange={() => setNumberAllowed((prev) => !prev)} className='ml-3 cursor-pointer' />
+              <label htmlFor='numbers' className='ml-1  cursor-pointer'>Numbers</label>
+            </div>
+            <div>
+              <input type="checkbox" id='chars' defaultChecked={charactersAllowed} onChange={() => setCharactersAllowed((prev) => !prev)} className='ml-3 cursor-pointer' />
+              <label htmlFor='chars' className='ml-1 cursor-pointer'>Special Characters</label>
+            </div>
+          </div>
+          <button id='copy-btn' className='bg-[#1D4ED8] text-white px-2 w-full mt-5 py-2' onClick={() => copyPasswordToClipboard()}>Copy</button>
         </div>
       </div>
     </>
@@ -65,3 +72,5 @@ function App() {
 }
 
 export default App
+
+
